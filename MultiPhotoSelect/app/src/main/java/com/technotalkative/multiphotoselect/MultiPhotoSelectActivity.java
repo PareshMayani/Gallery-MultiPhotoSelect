d56bfd15d0c9e61ee4cc217d7eb8baa991691100
PareshMayani <pareshmayani@gmail.com>
2016-06-21 15:13:25 +0530

    Re-factor GridView code with RecyclerView having GridLayoutManager

--- MultiPhotoSelect/app/src/main/java/com/technotalkative/multiphotoselect/MultiPhotoSelectActivity.java
+++ MultiPhotoSelect/app/src/main/java/com/technotalkative/multiphotoselect/MultiPhotoSelectActivity.java
@@ -1,67 +1,58 @@
 package com.technotalkative.multiphotoselect;
 
-import java.util.ArrayList;
-
-import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.util.SparseBooleanArray;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.BaseAdapter;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.Toast;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.GridView;
-import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 /**
  * @author Paresh Mayani (@pareshmayani)
  */
 public class MultiPhotoSelectActivity extends AppCompatActivity {
 
-	private ArrayList<String> imageUrls;
 	private ImageAdapter imageAdapter;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.ac_image_grid);
+		setContentView(R.layout.layout_multi_photo_select);
 
+		ArrayList<String> imageUrls = loadPhotosFromNativeGallery();
+		imageAdapter = new ImageAdapter(this, imageUrls);
+
+		RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
+		RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+		recyclerView.setLayoutManager(layoutManager);
+		recyclerView.setItemAnimator(new DefaultItemAnimator());
+		recyclerView.setAdapter(imageAdapter);
+	}
+
+	private ArrayList<String> loadPhotosFromNativeGallery() {
 		final String[] columns = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
 		final String orderBy = MediaStore.Images.Media.DATE_TAKEN;
 		Cursor imagecursor = managedQuery(
 				MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null,
 				null, orderBy + " DESC");
-		
-		this.imageUrls = new ArrayList<String>();
-		
+
+		ArrayList<String> imageUrls = new ArrayList<String>();
+
 		for (int i = 0; i < imagecursor.getCount(); i++) {
 			imagecursor.moveToPosition(i);
 			int dataColumnIndex = imagecursor.getColumnIndex(MediaStore.Images.Media.DATA);
 			imageUrls.add(imagecursor.getString(dataColumnIndex));
-			
+
 			System.out.println("=====> Array path => "+imageUrls.get(i));
 		}
-		
 
-		imageAdapter = new ImageAdapter(this, imageUrls);
-		
-		GridView gridView = (GridView) findViewById(R.id.gridview);
-		gridView.setAdapter(imageAdapter);
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
+		return imageUrls;
 	}
 
 	public void btnChoosePhotosClick(View v){
