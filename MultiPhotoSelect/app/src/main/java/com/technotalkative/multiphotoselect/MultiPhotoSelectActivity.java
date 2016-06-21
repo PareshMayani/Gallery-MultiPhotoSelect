aff031c3a5127fbadb7941c616dbf05e4f97c8e4
PareshMayani <pareshmayani@gmail.com>
2016-06-21 17:10:33 +0530

    implement run time permission model for READ STORAGE PERMISSION

--- MultiPhotoSelect/app/src/main/java/com/technotalkative/multiphotoselect/MultiPhotoSelectActivity.java
+++ MultiPhotoSelect/app/src/main/java/com/technotalkative/multiphotoselect/MultiPhotoSelectActivity.java
@@ -1,8 +1,13 @@
 package com.technotalkative.multiphotoselect;
 
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
@@ -13,26 +18,73 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
 /**
  * @author Paresh Mayani (@pareshmayani)
  */
 public class MultiPhotoSelectActivity extends AppCompatActivity {
 
 	private ImageAdapter imageAdapter;
-	
+	private static final int REQUEST_FOR_STORAGE_PERMISSION = 123;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_multi_photo_select);
 
-		ArrayList<String> imageUrls = loadPhotosFromNativeGallery();
-		imageAdapter = new ImageAdapter(this, imageUrls);
+		populateImagesFromGallery();
+	}
 
-		RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
-		RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-		recyclerView.setLayoutManager(layoutManager);
-		recyclerView.setItemAnimator(new DefaultItemAnimator());
-		recyclerView.setAdapter(imageAdapter);
+	public void btnChoosePhotosClick(View v){
+		
+		ArrayList<String> selectedItems = imageAdapter.getCheckedItems();
+		Toast.makeText(MultiPhotoSelectActivity.this, "Total photos selected: "+selectedItems.size(), Toast.LENGTH_SHORT).show();
+		Log.d(MultiPhotoSelectActivity.class.getSimpleName(), "Selected Items: " + selectedItems.toString());
+	}
+
+	private void populateImagesFromGallery() {
+		if (!mayRequestGalleryImages()) {
+			return;
+		}
+
+		ArrayList<String> imageUrls = loadPhotosFromNativeGallery();
+		initializeRecyclerView(imageUrls);
+	}
+
+	private boolean mayRequestGalleryImages() {
+
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+			return true;
+		}
+
+		if (checkSelfPermission(READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+			return true;
+		}
+
+		if (shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE)) {
+			//promptStoragePermission();
+			showPermissionRationaleSnackBar();
+		} else {
+			requestPermissions(new String[]{READ_EXTERNAL_STORAGE}, REQUEST_FOR_STORAGE_PERMISSION);
+		}
+
+		return false;
+	}
+
+	/**
+	 * Callback received when a permissions request has been completed.
+	 */
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+										   @NonNull int[] grantResults) {
+		if (requestCode == REQUEST_FOR_STORAGE_PERMISSION) {
+			if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				populateImagesFromGallery();
+			} else if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+				showPermissionRationaleSnackBar();
+			}
+		}
 	}
 
 	private ArrayList<String> loadPhotosFromNativeGallery() {
@@ -55,11 +107,27 @@ public class MultiPhotoSelectActivity extends AppCompatActivity {
 		return imageUrls;
 	}
 
-	public void btnChoosePhotosClick(View v){
-		
-		ArrayList<String> selectedItems = imageAdapter.getCheckedItems();
-		Toast.makeText(MultiPhotoSelectActivity.this, "Total photos selected: "+selectedItems.size(), Toast.LENGTH_SHORT).show();
-		Log.d(MultiPhotoSelectActivity.class.getSimpleName(), "Selected Items: " + selectedItems.toString());
+	private void initializeRecyclerView(ArrayList<String> imageUrls) {
+		imageAdapter = new ImageAdapter(this, imageUrls);
+
+		RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
+		RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+		recyclerView.setLayoutManager(layoutManager);
+		recyclerView.setItemAnimator(new DefaultItemAnimator());
+		recyclerView.setAdapter(imageAdapter);
 	}
 
+	private void showPermissionRationaleSnackBar() {
+		Snackbar.make(findViewById(R.id.button1), getString(R.string.permission_rationale),
+				Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// Request the permission
+				ActivityCompat.requestPermissions(MultiPhotoSelectActivity.this,
+						new String[]{READ_EXTERNAL_STORAGE},
+						REQUEST_FOR_STORAGE_PERMISSION);
+			}
+		}).show();
+
+	}
 }
